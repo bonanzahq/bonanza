d2364f150857d721e6ebef84a0f50efd966780ec
fmzbot <o_o@fmz.one>
2026-02-16 14:21:55 +0100

    revert: keep Verwaltung link in user dropdown, remove broken nav placement
    The nav element added to the layout was unstyled and poorly positioned.
    The broader navigation needs a proper redesign (filed as git-bug 8a60474).
    Restore Verwaltung link in dropdown using route helper instead of hardcoded path.

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,5 +1,5 @@
-# ABOUTME: Tests for main navigation layout elements.
-# ABOUTME: Verifies Verwaltung link appears in main nav for authorized users.
+# ABOUTME: Tests for navigation elements in the layout.
+# ABOUTME: Verifies Verwaltung link appears in user dropdown for authorized users.
 
 require "test_helper"
 
@@ -8,40 +8,22 @@ class NavigationTest < ActionDispatch::IntegrationTest
     @department = create(:department)
   end
 
-  test "admin sees Verwaltung link in main navigation" do
+  test "admin sees Verwaltung link in user dropdown" do
     admin = create(:user, :admin, department: @department)
     sign_in admin
 
-    get lending_path
+    get verwaltung_verleihende_path
     assert_response :success
-    assert_select "nav.main-nav a[href='/verwaltung']", text: "Verwaltung"
+    assert_select ".dropdown-menu a[href='/verwaltung']", text: "Verwaltung"
   end
 
-  test "leader sees Verwaltung link in main navigation" do
-    leader = create(:user, :leader, department: @department)
-    sign_in leader
-
-    get lending_path
-    assert_response :success
-    assert_select "nav.main-nav a[href='/verwaltung']", text: "Verwaltung"
-  end
-
-  test "member does not see Verwaltung link in main navigation" do
+  test "member does not see Verwaltung link in user dropdown" do
     member = create(:user, department: @department)
     sign_in member
 
-    get lending_path
-    assert_response :success
-    assert_select "nav.main-nav a[href='/verwaltung']", false, "Member should not see Verwaltung link"
-  end
-
-  test "Verwaltung link is not in user dropdown menu" do
-    admin = create(:user, :admin, department: @department)
-    sign_in admin
-
-    get lending_path
+    get verwaltung_verleihende_path
     assert_response :success
     assert_select ".dropdown-menu a[href='/verwaltung']", false,
-      "Verwaltung link should not be in the user dropdown"
+      "Member should not see Verwaltung link"
   end
 end
