e13d5c4d31600f4d0389d91e950a0e45e2d44d13
fmzbot <o_o@fmz.one>
2026-02-18 15:26:04 +0100

    test(conduct): add factory traits for expiration scenarios

--- test/factories/conducts.rb
+++ test/factories/conducts.rb
@@ -14,5 +14,22 @@ FactoryBot.define do
     trait :banned do
       kind { :banned }
     end
+
+    trait :with_duration do
+      permanent { false }
+      duration { 14 }
+    end
+
+    trait :automatic do
+      user { nil }
+      permanent { false }
+      duration { nil }
+    end
+
+    trait :expired do
+      permanent { false }
+      duration { 1 }
+      created_at { 2.days.ago }
+    end
   end
 end
