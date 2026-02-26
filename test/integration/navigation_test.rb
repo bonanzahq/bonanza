57a3b07b220c85c070b5ce1d770bdda99241d97c
fmzbot <o_o@fmz.one>
2026-02-26 11:39:16 +0100

    test(departments): add tests for management views and navigation link

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -44,4 +44,32 @@ class NavigationTest < ActionDispatch::IntegrationTest
     assert_select ".dropdown-menu a[href='/verwaltung']", false,
       "Guest should not see Verwaltung link"
   end
+
+  test "admin sees Werkstätten verwalten link on verwaltung page" do
+    admin = create(:user, :admin, department: @department)
+    sign_in admin
+
+    get borrowers_path
+    assert_response :success
+    assert_select "a[href='#{departments_path}']", text: "Werkstätten verwalten"
+  end
+
+  test "leader sees Werkstätten verwalten link on verwaltung page" do
+    leader = create(:user, :leader, department: @department)
+    sign_in leader
+
+    get borrowers_path
+    assert_response :success
+    assert_select "a[href='#{departments_path}']", text: "Werkstätten verwalten"
+  end
+
+  test "member does not see Werkstätten verwalten link on verwaltung page" do
+    member = create(:user, department: @department)
+    sign_in member
+
+    get borrowers_path
+    assert_response :success
+    assert_select "a[href='#{departments_path}']", false,
+      "Member should not see Werkstätten verwalten link"
+  end
 end
