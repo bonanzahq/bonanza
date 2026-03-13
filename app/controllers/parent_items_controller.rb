8f1fbbc4c0e49d02e73e79fb3b4aae666c12cf7e
fmzbot <o_o@fmz.one>
2026-03-13 13:57:22 +0100

    fix(parent-items): remove all_tags_list from parent_item_params
    all_tags_list is a read-only acts_as_taggable accessor, not a writable
    model attribute. Including it in parent_item_params causes
    UnknownAttributeError when creating/updating parent items.
    
    Tags are handled separately via the tags_param method.

--- app/controllers/parent_items_controller.rb
+++ app/controllers/parent_items_controller.rb
@@ -175,7 +175,7 @@ class ParentItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def parent_item_params
-      params.require(:parent_item).permit(:name, :description, :note, :price, :all_tags_list, new_files: [], items_attributes: [:id, :uid, :quantity, :condition, :storage_location, :note, :_destroy], :accessories_attributes => [:id, :name, :_destroy], links_attributes: [:id, :url, :title, :_destroy])
+      params.require(:parent_item).permit(:name, :description, :note, :price, new_files: [], items_attributes: [:id, :uid, :quantity, :condition, :storage_location, :note, :_destroy], :accessories_attributes => [:id, :name, :_destroy], links_attributes: [:id, :url, :title, :_destroy])
     end
 
     def tags_param
